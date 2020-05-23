923f3c2a93e22c6767145cd2890567003b71f205
mattchupp <mattlchupp@gmail.com>
2020-05-23 11:19:13 -0500

    using swr to fetch data from local api, have the call, need to map to output now

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Head from 'next/head'
+import useSWR from 'swr'
 
 export default function Home() {
+
+  const fetcher = (url) => fetch(url).then((res) => res.json())
+
+  const { data, error } = useSWR('/api/tickets', fetcher)
+
+   if (error) return <div>Failed to load</div>
+   if (!data) return <div>Loading...</div>
+
+   console.log(data)
+
   return (
     <div className="container">
       <Head>
@@ -8,11 +19,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <ul>
-         {tickets.map((ticket) => (
-           <li key={ticket.id}>{ticket.ticket_title}</li>
-         ))}
-       </ul>
 
     </div>
   )
@@ -20,19 +26,33 @@ export default function Home() {
 }
 
 
+/*
+
+<ul>
+   {data.map((ticket) => (
+     <li key={ticket.id}>{ticket.ticket_title}</li>
+   ))}
+ </ul>
+
+
+*/
+
+/*
 
 export async function getStaticProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
-  const res = await fetch('http://localhost:4000/api/tickets')
+  const res = await fetch('http://localhost:3000/api/tickets')
   const tickets = await res.json()
 
   // By returning { props: posts }, the Blog component
   // will receive `posts` as a prop at build time
   return {
-    props: {
+    tickets: {
       id,
       ticket_title,
     },
   }
 }
+
+*/
