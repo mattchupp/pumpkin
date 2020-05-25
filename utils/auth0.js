c363a8d630a95520374a746673d7731db4f7d880
mattchupp <mattlchupp@gmail.com>
2020-05-24 22:11:03 -0500

    init auth0

---
+++ utils/auth0.js
@@ -0,0 +1,29 @@
+import { initAuth0 } from '@auth0/nextjs-auth0';
+import config from './config';
+
+export default initAuth0({
+  domain: process.env.domain,
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  scope: 'openid profile',
+  redirectUri: process.env.redirectUri,
+  postLogoutRedirectUri: process.env.postLogoutRedirectUri,
+  session: {
+    // The secret used to encrypt the cookie.
+    cookieSecret: process.env.cookieSecret,
+    // The cookie lifetime (expiration) in seconds. Set to 8 hours by default.
+    cookieLifetime: 60 * 60 * 8,
+    // (Optional) Store the id_token in the session. Defaults to false.
+    storeIdToken: false,
+    // (Optional) Store the access_token in the session. Defaults to false.
+    storeAccessToken: false,
+    // (Optional) Store the refresh_token in the session. Defaults to false.
+    storeRefreshToken: false
+  },
+  oidcClient: {
+    // (Optional) Configure the timeout in milliseconds for HTTP requests to Auth0.
+    httpTimeout: 2500,
+    // (Optional) Configure the clock tolerance in milliseconds, if the time on your server is running behind.
+    clockTolerance: 10000
+  }
+});
