d4be3b18895acb18d5a7303b50b84b7e51f41eb3
mattchupp <mattlchupp@gmail.com>
2020-05-25 08:31:15 -0500

    auth0 is working!

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,15 @@ export default function Home() {
   const { user, loading } = useFetchUser();
   console.log(user, loading);
 
+
+
     return (
       <div className="container">
         <Head>
           <title>Pumpkin | Home</title>
         </Head>
 
+  
 
       </div>
   )
