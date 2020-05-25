98ba4c11c9674f1c4d1576f2083ce0ce26e5741f
mattchupp <mattlchupp@gmail.com>
2020-05-25 08:46:02 -0500

    simple condition for if logged in/ or not logged in

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Head from 'next/head';
-import { useFetchUser } from '../utils/user';
+import { UserProvider, useFetchUser } from '../utils/user';
 
 export default function Home() {
 
   const { user, loading } = useFetchUser();
   console.log(user, loading);
+
+  if(user) {
+    return (
+      <UserProvider value={{ user, loading }}>
+
+          <p>{user.nickname}</p>
+
+      </UserProvider>
+    )
+  } else {
+    return (
+      <p>Please login</p>
+    )
+  }
 
 
 
@@ -14,9 +28,9 @@ export default function Home() {
           <title>Pumpkin | Home</title>
         </Head>
 
-  
-
       </div>
   )
+
+
 
 }
