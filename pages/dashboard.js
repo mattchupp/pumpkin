533b5d145be06f08abf11d8e726e448212cdd316
mattchupp <mattlchupp@gmail.com>
2020-05-25 23:48:10 -0500

    added link to backend api

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -6,42 +6,44 @@ import { UserProvider, useFetchUser } from '../utils/user';
 
 export default function Home() {
 
+  /* authenticate user */
   const { user, loading } = useFetchUser();
   console.log(user, loading);
 
 
-
+  /* Fetch tickets */
   const fetcher = (url) => fetch(url).then((res) => res.json())
 
-  const { data, error } = useSWR('/api/tickets', fetcher)
+  // const { data, error } = useSWR('/api/tickets', fetcher)
+  const { data, error } = useSWR('http://localhost:4000/tickets', fetcher)
 
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
 
   console.log(data)
 
-  if(user) {
-    return (
 
+  /*
+  * If user is signed in, pull from tickets api and return tickets
+  * Else, show nothing and require that the user signs in
+  */
+  if(user) {
+
+    return (
       <div className="container">
         <Head>
           <title>Pumpkin | Home</title>
         </Head>
+        <p>Signed in as {user.name}</p>
 
-
-
-         {data.tickets.map((ticket) => (
-            <Ticket
-              key={ticket.id}
-              title={ticket.ticket_title}
-              description={ticket.ticket_description}
-              creator={ticket.ticket_creator}
-            />
-         ))}
-
-         <p>Signed in as {user.name}</p>
-
-
+        {data.map((ticket) => (
+          <Ticket
+            key={ticket._id}
+            title={ticket.ticket_title}
+            description={ticket.ticket_description}
+            creator={ticket.ticket_creator}
+          />
+        ))}
       </div>
     )
   } else {
