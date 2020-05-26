f8b15e9d4faceefdb3936d2cbecb856b5cec46a9
mattchupp <mattlchupp@gmail.com>
2020-05-25 21:16:39 -0500

    added authentication for dashboard, have to be signed in

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,15 @@
 import Head from 'next/head';
 import useSWR from 'swr';
 import Ticket from '../components/Ticket';
-
+import { UserProvider, useFetchUser } from '../utils/user';
 
 
 export default function Home() {
+
+  const { user, loading } = useFetchUser();
+  console.log(user, loading);
+
+
 
   const fetcher = (url) => fetch(url).then((res) => res.json())
 
@@ -15,29 +20,35 @@ export default function Home() {
 
   console.log(data)
 
+  if(user) {
+    return (
 
-
-  return (
-    <div className="container">
-      <Head>
-        <title>Pumpkin | Home</title>
-      </Head>
-
-
-
-       {data.tickets.map((ticket) => (
-          <Ticket
-            key={ticket.id}
-            title={ticket.ticket_title}
-            description={ticket.ticket_description}
-            creator={ticket.ticket_creator}
-          />
-       ))}
+      <div className="container">
+        <Head>
+          <title>Pumpkin | Home</title>
+        </Head>
 
 
 
+         {data.tickets.map((ticket) => (
+            <Ticket
+              key={ticket.id}
+              title={ticket.ticket_title}
+              description={ticket.ticket_description}
+              creator={ticket.ticket_creator}
+            />
+         ))}
 
-    </div>
-  )
+         <p>Signed in as {user.name}</p>
+
+
+      </div>
+    )
+  } else {
+    return (
+      <p>Please sign in</p>
+    )
+  }
+
 
 }
