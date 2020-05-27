d4a9987348413e0aaa8363311a2262f5a2f85439
mattchupp <mattlchupp@gmail.com>
2020-05-27 11:09:50 -0500

    get a single ticket by id

--- components/TicketView.js
+++ components/TicketView.js
@@ -9,23 +9,24 @@ export default function TicketView(props) {
   // Fetch tickets
   const fetcher = (url) => fetch(url).then((res) => res.json())
 
-  const { data, error } = useSWR('http://localhost:4000/tickets', fetcher)
+  const { data, error } = useSWR('http://localhost:4000/tickets/5ecdb5a7f703f6027412ce08', fetcher)
 
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
 
-  // console.log(data)
+  console.log(data)
 
-  const getTicketById = data.filter((ticket) => {
-    if(data._id === '5ecdb8e5f703f6027412ce09') {
-      console.log(data)
-    }
-  })
+  // const getTicketById = data.filter((ticket) => {
+  //   if(data._id === '5ecdb8e5f703f6027412ce09') {
+  //     console.log(data)
+  //   }
+  // })
 
   return (
     <div>
-
-
+  
+        {data.ticket_title}
+           
     </div>
   )
 }
