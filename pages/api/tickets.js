9343e4d33605484f2b528ae0dd442d62f030acd4
mattchupp <mattlchupp@gmail.com>
2020-05-27 11:49:51 -0500

    api for post tickets, not working currently

---
+++ pages/api/tickets.js
@@ -0,0 +1,19 @@
+/*
+
+export default async function tickets(req, res) => {
+
+  // Processing post request
+  res.setHeader('Content-Type', 'application/json')
+  if(req.method === 'POST') {
+    try {
+
+    } catch (error) {
+      console.log(error);
+      res.status(error.status || 500).end(error.message);
+    }
+  }
+
+}
+
+
+*/
