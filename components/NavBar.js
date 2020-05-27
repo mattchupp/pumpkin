c3174f8fc46ebd26a3eb72dde74c53b05b22ac81
mattchupp <mattlchupp@gmail.com>
2020-05-26 21:47:41 -0500

    added navbar

---
+++ components/NavBar.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+export default function NavBar(props) {
+  return (
+    <nav className="navbar bg-dark navbar-dark">
+      <Link href="/"><a className="navbar-brand">Pumpkin</a></Link>
+      <div className="d-flex justify-content-end" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              id="userDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              {props.user}
+            </a>
+
+            <div className="dropdown-menu" aria-labelledby="userDropdown">
+              <Link href="/api/logout"><a>Sign Out</a></Link>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
-import TicketsTable from '../components/TicketsTable.js'
+import TicketsTable from '../components/TicketsTable.js';
 import { UserProvider, useFetchUser } from '../utils/user';
+import NavBar from '../components/NavBar.js';
 
 
 export default function Dashboard() {
@@ -17,7 +18,7 @@ export default function Dashboard() {
   if(user) {
 
     return (
-      <div className="container">
+      <div>
         <Head>
           <title>Pumpkin | Dashboard</title>
           <link
@@ -26,10 +27,17 @@ export default function Dashboard() {
             integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
             crossorigin="anonymous"
           />
-        </Head>
-        <p>Signed in as {user.name}</p>
 
-        <TicketsTable />
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
+        </Head>
+
+        <NavBar user={user.name}/>
+
+        <div className="container">
+          <TicketsTable />
+        </div>
 
       </div>
     )
