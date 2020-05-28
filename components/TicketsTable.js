631af5f985968ddd6f213605b1381e673a7bb7af
mattchupp <mattlchupp@gmail.com>
2020-05-28 16:06:49 -0500

    changed to class component

--- components/TicketsTable.js
+++ components/TicketsTable.js
@@ -1,18 +1,113 @@
 /* Show the tickets in a table */
 
-/* Show the tickets in a table */
-
 import TicketRow from './TicketRow';
 import TicketView from '../components/TicketView';
 import useSWR from 'swr';
+import axios from 'axios';
+import React, { Component } from 'react';
 
+
+/* TODO
+ [x] Change this from functional to class component
+ [x] Change from swr to axios and get json when component mounts
+ [ ] Allow each row to be selected and pass the id to ticketview
+*/
+
+class TicketsTable extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      ticketsTable: [],
+      viewTicket: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  componentDidMount() {
+
+    axios.get('http://localhost:4000/tickets')
+    .then(response => {
+      // console.log(response)
+      this.setState({ ticketsTable: response.data })
+      // console.log(this.state.ticketsTable)
+    })  
+    .catch(error => {
+      console.log(error)
+    })
+    
+  }
+
+  handleClick(event) {
+    alert('Hello, you clicked!')
+  }
+
+  render() {
+    let ticketsTable = this.state.ticketsTable
+    // console.log(ticketsTable)
+
+    let filteredTickets = ticketsTable.filter((ticket) => {
+      return ticket.ticket_status.indexOf('Open') !== -1;
+    })
+
+    return (
+      <div>
+        <table className="table table-striped table-bordered mt-3">
+          <thead className="thead-dark">
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Assignee</th>
+              <th>Creator</th>
+              <th>Date Created</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredTickets.map((ticket) => (
+              <TicketRow
+                key={ticket._id}
+                title={ticket.ticket_title}
+                description={ticket.ticket_description}
+                creator={ticket.ticket_creator}
+                assignee={ticket.ticket_owner}
+                date={ticket.date_created}
+                status={ticket.ticket_status}
+              />
+            ))}
+          </tbody>
+      </table>
+
+        <TicketView id="5ecdb5a7f703f6027412ce08"/>
+      </div>
+    )
+  }
+}
+
+
+export default TicketsTable
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 export default function TicketTable(props) {
 
-/*
-  const fetcher = (url) => fetch(url).then((res) => res.json())
-  const initialData = props.data
-  const { data, error } = useSWR('http://localhost:4000/tickets', fetcher, { initialData })
-*/
+
+  // const fetcher = (url) => fetch(url).then((res) => res.json())
+  // const initialData = props.data
+  // const { data, error } = useSWR('http://localhost:4000/tickets', fetcher, { initialData })
 
 
   // Fetch tickets
@@ -26,7 +121,7 @@ export default function TicketTable(props) {
 
   // console.log(data)
 
-  /* Filtering out completed tickets */
+  // Filtering out completed tickets 
   let filteredTickets = data.filter((ticket) => {
     return ticket.ticket_status.indexOf('Open') !== -1;
   })
@@ -68,4 +163,4 @@ export default function TicketTable(props) {
   )
 }
 
-
+*/
