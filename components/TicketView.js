93c406bdc226bd9c144d4688feb449cb8dabb562
mattchupp <mattlchupp@gmail.com>
2020-05-28 17:00:03 -0500

    cleanup

--- components/TicketView.js
+++ components/TicketView.js
@@ -27,7 +27,7 @@ class TicketView extends Component {
   componentDidMount() {
 
     let apiCall = 'http://localhost:4000/tickets/' + this.props.id;
-    
+
     axios.get(apiCall)
     .then(response => {
 
@@ -65,42 +65,3 @@ class TicketView extends Component {
 
 export default TicketView
 
-
-
-
-
-
-
-/*
-export default function TicketView(props){
-  // const res = await fetch('https://api.github.com/repos/zeit/next.js')
-  // const json = await res.json()
-  // return { stars: json.stargazers_count }
-
-  const id = props.id;
-  
-  // const res = await fetch('http://localhost:4000/tickets/')
-  // const json = await res.json()
-
-  // Take api link and add id from props id
-  const api = 'http://localhost:4000/tickets/' + id;
-
-  // Fetch tickets
-  const fetcher = (url) => fetch(url).then((res) => res.json())
-
-  const { data, error } = useSWR(api, fetcher)
-  
-
-  return (
-    <div>
-      
-      <h3>{data.ticket_title}</h3>
-      <p>{data.ticket_description}</p>  
-   
-    </div>
-  )
-  
-  
-}
-
-*/
