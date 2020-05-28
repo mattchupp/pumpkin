4661a2625d666c9baf2cc88894aa4b7d6b7ec171
mattchupp <mattlchupp@gmail.com>
2020-05-28 13:54:55 -0500

    changed to class component

--- components/TicketView.js
+++ components/TicketView.js
@@ -1,31 +1,80 @@
-import useSWR from 'swr';
+import React, { Component } from 'react';
+import axios from 'axios';
+// import { GetServerSideProps } from 'next';
 
 /*
   This will display information about each ticket when selected from the table
 */
 
-export default function TicketView(props) {
+class TicketView extends Component {
+  constructor() {
+    super(); 
+    this.state = {
+      ticket: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/tickets/' + this.props.id)
+    .then(response => {
+      // console.log(response)
+      this.setState({ ticket: response })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+
+  render() {
+    console.log(this.state.ticket)
+    
+    return (
+      <div>
+      
+      </div>
+    )
+  }
+}
+
+export default TicketView
+
+
+
+
+
+
+
+/*
+export default function TicketView(props){
+  // const res = await fetch('https://api.github.com/repos/zeit/next.js')
+  // const json = await res.json()
+  // return { stars: json.stargazers_count }
 
   const id = props.id;
+  
+  // const res = await fetch('http://localhost:4000/tickets/')
+  // const json = await res.json()
+
   // Take api link and add id from props id
   const api = 'http://localhost:4000/tickets/' + id;
-
-  // console.log(api);
 
   // Fetch tickets
   const fetcher = (url) => fetch(url).then((res) => res.json())
 
   const { data, error } = useSWR(api, fetcher)
-
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
-
-  // console.log(data)
+  
 
   return (
     <div>
+      
       <h3>{data.ticket_title}</h3>
-      <p>{data.ticket_description}</p>      
+      <p>{data.ticket_description}</p>  
+   
     </div>
   )
+  
+  
 }
+
+*/
