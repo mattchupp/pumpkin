bf09cb07012f2dd5e6e69b4ba5e7951391e8b510
mattchupp <mattlchupp@gmail.com>
2020-05-29 21:48:15 -0500

    changed name

---
+++ src/newticket.js
@@ -0,0 +1,23 @@
+// import Head from 'next/head';
+import TicketForm from './components/TicketForm';
+import React from 'react';
+
+
+export default function NewTicket() {
+
+
+    return (
+      <div>
+        <div className="container">
+          
+          <br />
+
+          <TicketForm />
+
+        </div>
+      </div>
+  )
+
+
+
+}
