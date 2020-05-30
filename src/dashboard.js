c0e31eede3d57b9788d3f12a1570cabda6fccbaf
mattchupp <mattlchupp@gmail.com>
2020-05-29 21:51:33 -0500

    cleanup

--- src/dashboard.js
+++ src/dashboard.js
@@ -21,17 +21,8 @@ export default function Dashboard () {
 
     return (
       <div>
-        {/* <Head>
-          <title>Pumpkin | Dashboard</title>
-          <link
-            rel="stylesheet"
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-            crossorigin="anonymous"
-          />  
-        </Head> */}
 
-        <NavBar/>
+        <NavBar user="Matt"/>
 
         <div className="container-fluid">
           <TicketsTable />
@@ -61,13 +52,3 @@ export default function Dashboard () {
 
 
 }
-
-
-
-/*
-bootstrap script 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
-
-*/
