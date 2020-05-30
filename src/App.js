59f835a5be0629cee634d70a015b9951ca553084
mattchupp <mattlchupp@gmail.com>
2020-05-29 21:10:14 -0500

    added route to dashbaord

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Dashboard from './dashboard'; 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Router>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+
+        </Switch>
+      </Router>
+      
+      
     </div>
   );
 }
