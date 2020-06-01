b51e3ebfb83b9113fe66c42a34e4d1d7e7819dd6
mattchupp <mattlchupp@gmail.com>
2020-05-31 22:04:17 -0500

    private route for dashbaord

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,14 +3,13 @@ import TicketsTable from '../components/TicketsTable.js';
 // import { UserProvider, useFetchUser } from '../utils/user';
 import NavBar from '../components/NavBar.js';
 import React from 'react';
+// import { useAuth0 } from "../react-auth0-spa";
 // import Link from 'next/link'
 
 
 export default function Dashboard () {
 
-  /* authenticate user */
-  // const { user, loading } = useFetchUser();
-  // console.log(user, loading);
+ 
 
 
   /*
